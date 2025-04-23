6b4d67259679f0a93a5fbb59668f50f7f80b3505
Royzou1 <royeezo@gmail.com>
2025-04-23 11:46:58 +0300

    updated version

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -13,23 +13,17 @@ using std::ofstream;
 struct routine_attributes {
     string image_name;
     ADDRINT image_address;
-    string name;              
+    string name;
     ADDRINT address;
-    UINT64 instruction_count = 0;
     UINT64 number_of_times_called = 0;
 };
 
-map<RTN, routine_attributes> routineMap;
+map<ADDRINT, routine_attributes> routineMap;
 ofstream OutFile;
 
-// Count instructions executed in a routine
-VOID CountInstruction(RTN rtn) {
-    routineMap[rtn].instruction_count++;
-}
-
 // Count number of times the routine is called
-VOID RoutineEntry(RTN rtn) {
-    routineMap[rtn].number_of_times_called++;
+VOID RoutineEntry(ADDRINT addr) {
+    routineMap[addr].number_of_times_called++;
 }
 
 // Instrument each routine
@@ -37,49 +31,47 @@ VOID Routine(RTN rtn, VOID* v) {
     if (!RTN_Valid(rtn)) return;
 
     RTN_Open(rtn);
-    routine_attributes attr;
 
     IMG img = SEC_Img(RTN_Sec(rtn));
+    if (!IMG_IsMainExecutable(img)) {
+        RTN_Close(rtn);
+        return;
+    }
+
+    routine_attributes attr;
     attr.image_name = IMG_Name(img);
     attr.image_address = IMG_LoadOffset(img);
     attr.name = RTN_Name(rtn);
     attr.address = RTN_Address(rtn);
 
-    routineMap[rtn] = attr;
+    routineMap[attr.address] = attr;
 
-    // Track each routine call
-    RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)RoutineEntry, IARG_PTR, rtn, IARG_END);
-
-    // Track instruction execution within routine
-    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
-        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)CountInstruction, IARG_PTR, rtn, IARG_END);
-    }
+    RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)RoutineEntry, IARG_ADDRINT, attr.address, IARG_END);
 
     RTN_Close(rtn);
 }
 
-// Sort routines by instruction count descending
-bool SortByInstructions(const std::pair<RTN, routine_attributes>& a,
-                        const std::pair<RTN, routine_attributes>& b) {
-    return a.second.instruction_count > b.second.instruction_count;
+// Sort routines by number of calls descending
+bool SortByCalls(const std::pair<ADDRINT, routine_attributes>& a,
+                 const std::pair<ADDRINT, routine_attributes>& b) {
+    return a.second.number_of_times_called > b.second.number_of_times_called;
 }
 
 // Output to CSV at end
 VOID Fini(INT32 code, VOID* v) {
-    vector<std::pair<RTN, routine_attributes>> vec(routineMap.begin(), routineMap.end());
-    std::sort(vec.begin(), vec.end(), SortByInstructions);
+    vector<std::pair<ADDRINT, routine_attributes>> vec(routineMap.begin(), routineMap.end());
+    std::sort(vec.begin(), vec.end(), SortByCalls);
 
     OutFile.open("rtn-output.csv");
     for (const auto& entry : vec) {
         const routine_attributes& attr = entry.second;
-        if (attr.instruction_count == 0) continue;
+        if (attr.number_of_times_called == 0) continue;
 
         OutFile << attr.image_name << ","
                 << "0x" << std::hex << attr.image_address << ","
                 << attr.name << ","
                 << "0x" << std::hex << attr.address << ","
-                << std::dec << attr.instruction_count << ","
-                << attr.number_of_times_called << "\n";
+                << std::dec << attr.number_of_times_called << "\n";
     }
     OutFile.close();
 }
