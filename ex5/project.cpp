bcbc43557311ddd90a63db09edd334ecd89b4653
Royzou1 <royeezo@gmail.com>
2025-10-08 18:31:50 +0300

    m

--- ex5/project.cpp
+++ ex5/project.cpp
@@ -1933,7 +1933,7 @@ void create_tc2_thread_func(void *v)
         xed_decoded_inst_t *xedd = INS_XedDec(instr_map[i].ins);
         xed_reg_enum_t targ_reg = xed_decoded_inst_get_reg(xedd, XED_OPERAND_REG0);
         cerr << xed_reg_enum_t2str(targ_reg) << endl;
-        cerr << "is_jump_reg_not_rax_rip" <<is_jump_reg_not_rax_rip(ins) << endl;
+        cerr << "is_jump_reg_not_rax_rip" <<is_jump_reg_not_rax_rip(instr_map[i].ins) << endl;
       }
  
       if (instr_map[i].indirect_profiled ) {
@@ -1950,7 +1950,7 @@ void create_tc2_thread_func(void *v)
         }
         if (((curr_bbl.targ_count[index] * 100) / total_jumps_counter) >= KnobProfileThreshold) {
           // jump reg | (reg != rax)
-          if (is_jump_reg_not_rax_rip(ins)) {
+          if (is_jump_reg_not_rax_rip(instr_map[i].ins)) {
 
           }
           
